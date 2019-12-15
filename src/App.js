7a6f2d85c0a62c03d105ccebc460fa80c6354e63
Helder Burato Berto <helder.burato@gmail.com>
2019-12-15 18:10:40 -0300

    feat: renamed files to lowercase and created simple init component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const App = () => <div>Welcome to the React Boilerplate</div>;
+import react from './assets/images/react.png';
+
+const App = () => (
+  <div className="app-container">
+    <img src={react} className="app-logo" alt="React Boilerplate" />
+
+    <h1 className="app-title">Welcome to the React Boilerplate</h1>
+  </div>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import '@babel/polyfill';
 
-import App from './App';
+import App from './app';
 import './styles.css';
 
-const renderApp = (Component) => render(<Component />, document.getElementById('root'));
+const renderApp = Component => render(<Component />, document.getElementById('root'));
 
 renderApp(App);
 
 if (module.hot) {
-  module.hot.accept('./App', () => renderApp(App));
+  module.hot.accept('./app', () => renderApp(App));
 }
